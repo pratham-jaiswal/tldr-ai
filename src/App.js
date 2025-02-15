7fa5a04e6d63ee08f10522ad283dc5177ffbb878
pratham-jaiswal <prathamj0502@gmail.com>
2025-02-15 14:36:39 +0530

    Add privacy policy, term & conditions, and footer.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import About from "./Components/about";
 import Cookies from "js-cookie";
 import { v4 as uuidv4 } from "uuid";
 import { useEffect } from "react";
+import PrivacyPolicy from "./privacyPolicy";
+import TermsConditions from "./termsConditions";
+import Footer from "./Components/footer";
 
 function App() {
   useEffect(() => {
@@ -33,8 +36,11 @@ function App() {
           <Route path="/tldr" element={<TLDR />} />
           <Route path="/about" element={<About />} />
           <Route path="/saved-tldrs" element={<SavedTLDRs />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-conditions" element={<TermsConditions />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
